3438e097935d2b9556682e581df8b186b9c14e32
AN SYU LI <yessir0621@gmail.com>
2025-01-08 00:06:18 +0800

    fix: paths

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="ML-toolkit",                
-    version="0.1.2",                 
+    version="0.1.3",                 
     author="AnSyu Li",
     author_email="yessir0621@gmail.com",
     url="https://github.com/yourusername/ml-toolkit",
