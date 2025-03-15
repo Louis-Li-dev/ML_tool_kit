c8d1836e16dbe832ac984e49094631d41c13e17d
AN SYU LI <yessir0621@gmail.com>
2025-03-16 02:47:41 +0800

    docs: version change

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import setuptools
 
 setuptools.setup(
     name="ML-toolkit",                
-    version="0.4.9",                 
+    version="0.4.10",                 
     author="An-Syu Li",
     author_email="yessir0621@gmail.com",
     url="https://github.com/Louis-Li-dev/ML_tool_kit",
     packages=setuptools.find_packages(include=['mkit', 'mkit.*']),  
     install_requires=[
         "tqdm==4.66.5",
-        "torch==2.4.0", # mine torch==2.4.0+cu118
+        "torch", # mine torch==2.4.0+cu118
         "numpy==1.26.4",
         "pandas==2.2.3",
         "matplotlib==3.10.0",
